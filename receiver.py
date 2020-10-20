141d49e0f0adbc5e70c4358e250db73d7af86de8
Mr.V <vuhoang17891@gmail.com>
2020-10-20 12:10:28 +0700

    gpio port

--- receiver.py
+++ receiver.py
@@ -7,14 +7,14 @@ tmpNum = 0
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def userChoice():
     global tmpNum
     mac_address = get_mac()
     while 1:
-        if GPIO.input(23) == GPIO.HIGH:
+        if GPIO.input(23) == False:
             postRequest = requests.post('http://45.117.169.186:8000/api_1_0/first_data', data = {'mac_address':mac_address})
             return_data = postRequest.json().get('return_data')
             num = return_data.get('num')
@@ -26,7 +26,7 @@ def userChoice():
                 playResultNum(num)
                 playResult(result)
                 tmpNum = num
-        if GPIO.input(18) == GPIO.HIGH:
+        if GPIO.input(18) == False:
             if tmpNum == 0:
                 playResult('No answer yet')
             else:
