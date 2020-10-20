f531edb72a88e927cf360a971967857d59a2f62c
Mr.V <vuhoang17891@gmail.com>
2020-10-20 13:15:41 +0700

    button

--- receiver.py
+++ receiver.py
@@ -2,34 +2,18 @@ import requests, os
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from uuid import getnode as get_mac
 from gtts import gTTS
+from gpiozero import Button
 
 tmpNum = ''
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+B23 = Button(23)
+B18 = Button(18)
 
 def userChoice():
     global tmpNum
     mac_address = get_mac()
-    while 1:
-        GPIO.wait_for_edge(23, GPIO.FALLING)
-        postRequest = requests.post('http://45.117.169.186:8000/api_1_0/first_data', data = {'mac_address':mac_address})
-        return_data = postRequest.json().get('return_data')
-        num = return_data.get('num')
-        if num == 0:
-            playResult('No answer yet')
-        else:
-            result = return_data.get('result')
-            text = 'Câu ' + str(num) + ': ' + result
-            playViLanguage(text)
-            tmpNum = text
-        if GPIO.input(18) == False:
-            if tmpNum == '':
-                playViLanguage('Chưa có câu trả lời mới')
-            else:
-                playViLanguage(tmpNum)
+    B23.when_pressed = b23Pressed
+    B18.when_pressed = b18Pressed
 
 def playViLanguage(text):
     tts = gTTS(text=text, lang='vi')
@@ -37,6 +21,24 @@ def playViLanguage(text):
     os.system("mpg321 tmp.mp3")
 
 
+def b23Pressed():
+    postRequest = requests.post('http://45.117.169.186:8000/api_1_0/first_data', data = {'mac_address':mac_address})
+    return_data = postRequest.json().get('return_data')
+    num = return_data.get('num')
+    if num == 0:
+        playResult('No answer yet')
+    else:
+        result = return_data.get('result')
+        text = 'Câu ' + str(num) + ': ' + result
+        playViLanguage(text)
+        tmpNum = text
+
+
+def b18Pressed():
+    if tmpNum == '':
+        playViLanguage('Chưa có câu trả lời mới')
+    else:
+        os.system("mpg321 tmp.mp3")
 
 if __name__ == '__main__':
     userChoice()
