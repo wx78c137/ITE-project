42fbb709c03272fc48b7de7010675d1748d8367d
Mr.V <vuhoang17891@gmail.com>
2020-10-20 12:43:17 +0700

    Vi

--- receiver.py
+++ receiver.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from uuid import getnode as get_mac
 from gtts import gTTS
 
-tmpNum = 0
+tmpNum = ''
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
@@ -18,33 +18,24 @@ def userChoice():
             postRequest = requests.post('http://45.117.169.186:8000/api_1_0/first_data', data = {'mac_address':mac_address})
             return_data = postRequest.json().get('return_data')
             num = return_data.get('num')
-            print(num)
             if num == 0:
                 playResult('No answer yet')
             else:
                 result = return_data.get('result')
-                playResultNum(num)
-                playResult(result)
+                text = 'Câu ' + str(num) + ': ' + result
+                playViLanguage(text)
                 tmpNum = num
         if GPIO.input(18) == False:
-            if tmpNum == 0:
+            if tmpNum == '':
                 playResult('No answer yet')
             else:
-                playResultNum(tmpNum)
-                os.system("omxplayer tmp.mp3")
+                playViLanguage(tmpNum)
 
-
-def playResultNum(num):
-    text = 'Câu '+ str(num)
+def playViLanguage(text):
     tts = gTTS(text=text, lang='vi')
-    tts.save("tmp2.mp3")
-    os.system("omxplayer tmp2.mp3")
-
-
-def playResult(result):
-    tts = gTTS(text=result, lang='en')
     tts.save("tmp.mp3")
-    os.system("omxplayer tmp.mp3")
+    os.system("mpg321 tmp.mp3")
+
 
 
 if __name__ == '__main__':
