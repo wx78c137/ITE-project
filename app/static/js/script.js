1edb907252057da7a47e13f9484512954ed60e87
Mr.V <vuhoang17891@gmail.com>
2020-11-20 17:20:59 +0700

    date

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -24,8 +24,3 @@ socket.on('newData', function() {
     },
   });
 });
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
